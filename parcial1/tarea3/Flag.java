276fd2300fc0314f0739031853c63dd3d7207a33
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-10-04 18:31:51 -0500

    se agrego Range.java

---
+++ parcial1/tarea3/Flag.java
@@ -0,0 +1,13 @@
+//Flag.java
+//Autor: Emmanuel Reyes
+//Fecha: 04 Septiembre 2022
+//Institución: Universidad Veracruzana
+
+class Flag {  //Name of program
+    public static void main(String[] args) {
+          
+      boolean flag = false; 
+      System.out.println;x{"Valor de flag"};        //define una variable
+      System.out.println(flag);    // prints true
+    }
+  }
