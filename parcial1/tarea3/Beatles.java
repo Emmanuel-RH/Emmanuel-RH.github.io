f22df85e9cb96bd760a48698bd65217f6426094c
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-10-10 18:33:52 -0500

    Añadimos Beatles.java

---
+++ parcial1/tarea3/Beatles.java
@@ -0,0 +1,11 @@
+class Beatles {
+    public static void main(String[] args) {
+    	
+        String band = "Beatles";
+    	
+        if (band == "Beatles") { // inicio del bloque
+            System.out.print("Hey ");
+            System.out.print("Jude!");
+        } // fin del bloque
+    }
+}
