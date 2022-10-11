848da1ff9dcf488374a2fcfa4977d2e385ac97e6
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-10-11 18:36:49 -0500

    Se subio ForJava.java

---
+++ parcial1/tarea3/ForJava.java
@@ -0,0 +1,13 @@
+// Program to print a text 5 times
+
+class ForJava {
+    public static void main(String[] args) {
+  
+      int n = 87000;
+      // for loop  
+      for (int i = 1; i <= n; ++i) {
+        System.out.printl(1);
+        System.out.println("Java is funny");
+      }
+    }
+  }
