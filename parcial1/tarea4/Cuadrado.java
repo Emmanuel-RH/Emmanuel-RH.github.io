d6dc428e4742842a5a8049d9e15a3724c74233ab
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-10-27 10:44:45 -0500

    add index.html

--- parcial1/tarea4/Cuadrado.java
+++ parcial1/tarea4/Cuadrado.java
@@ -1,3 +1,6 @@
+//Programa 6
+//Capturar n, validar que sea un entero en el rango de 0 a 9 y usando ciclos producir un cuadrado de n dimension
+
 public class Cuadrado {
     
 }
--- parcial1/tarea4/Triangulo.java
+++ parcial1/tarea4/Triangulo.java
@@ -1,3 +1,6 @@
+//Programa 7
+//Capturar n, validar que sea un entero en el rango de 0 a 9 y usando ciclos producir un triangulo
+
 public class Triangulo {
     
 }
