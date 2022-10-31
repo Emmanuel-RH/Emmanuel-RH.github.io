454647132871480bf5a0f5f62b9d72cec0167eb6
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-10-31 17:12:43 -0600

    update Promedio and Triangulo .java

--- parcial1/tarea4/Promedio.java
+++ parcial1/tarea4/Promedio.java
@@ -1,51 +1,28 @@
 //Programa 4
 //Permitir la captura de numeros y realizar su promedio
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Promedio {
 
-    public static void prome(int iteracion) {
-
-        double [] array = new double [iteracion];
-        double entrada;
-
-        System.out.print("El promedio: ");
-
-        for (int i = 0; i<array.length; i++) {
-
-            entrada = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa los números a promediar: "));
-            array[i] = entrada;
-
-            if (i<array.length-1) {
-
-                System.out.print(array[i]+ " + ");
-
-            }else if (i == array.length-1) {
-
-                System.out.print(array[i]+ " = ");
-
-            } 
-        
-        }
-         
-        entrada = 0;
-
-        for (int i=0;i<array.length;i++) {
-
-            entrada += array[i];
-
-        }
-
-        System.out.print(entrada/array.length+ ".");
- 
-    }
-    
     public static void main(String[] args) {
-
-        int iteracion = Integer.parseInt(JOptionPane.showInputDialog(null, "¿Cuántos números desea promediar?"));
-        prome(iteracion);
-
+        
+        int cantidad, terminar = 0, num, promedio = 0;
+        float total = 0;
+        try (Scanner captura = new Scanner(System.in)) {
+            System.out.print("Digita la cantidad de numeros a promediar: ");
+            cantidad = captura.nextInt();
+            System.out.println("");
+            while (terminar  < cantidad) {
+                System.out.print("Digita un numero entero: ");
+                num = captura.nextInt();
+                if (num >= 1) {
+                    total += num;
+                    promedio++;
+                }
+                terminar++;
+            }
+        }
+        System.out.println("\nEl promedio total es: " + (total/promedio));
     }
-
 }
--- parcial1/tarea4/Triangulo.java
+++ parcial1/tarea4/Triangulo.java
@@ -1,35 +1,36 @@
 //Programa 7
 //Capturar n, validar que sea un entero en el rango de 0 a 9 y usando ciclos producir un triangulo
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Triangulo {
-
-    public static void trian(int entrada_datos) {
-
-        if (entrada_datos>=0 && entrada_datos<=9) {
-            System.out.println();
-
-          for(int altura = 1; altura<=entrada_datos; altura++){
+    
+    public static void dibujarTriangulo(int anchura) {
+        int asteriscos = 1;
+        int espacios = anchura - 1;
         
-            for(int blancos = 1; blancos<=entrada_datos-altura; blancos++){
+        for (int fila = 0; fila < anchura; fila++) {
+            for (int columnaEspacios = 0; columnaEspacios < espacios; columnaEspacios++)  {
                 System.out.print(" ");
             }
- 
-            for(int asteriscos=1; asteriscos<=(altura*2)-1; asteriscos++){
+            for (int columnaAsteriscos = 0; columnaAsteriscos < asteriscos; columnaAsteriscos++) {
                 System.out.print("*");
             }
             System.out.println();
-          }
-
-        }else {
-
-            JOptionPane.showMessageDialog(null, "Error \n El numero que usted ingreso no es valido");
+            
+            asteriscos ++;
+            espacios --;
         }
     }
-    public static void main(String[] args) {
-
-       int entrada_datos = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa en numeros el tamaño que deseas que tenga \n la impresión del triangulo en un rango de 0 a 9: "));
-       trian(entrada_datos);
+    
+    public static void main (String[] args) {
+        
+        int ancho;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Escribe el ancho del Triangulo: ");
+            ancho = sc.nextInt();
+        }
+        
+        dibujarTriangulo(ancho);
     }
 }
