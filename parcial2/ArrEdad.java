e0cb0d25948a82ba0a93ce62c9f0ef6dfd795bff
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-11-14 18:25:49 -0600

    add ArrEdad.java

---
+++ parcial2/ArrEdad.java
@@ -0,0 +1,33 @@
+package parcial2;
+
+public class ArrEdad {
+
+    public static void main (String arg[]) {
+
+    //declara un array
+    int[] age = new int[10];
+
+    //inicia el array
+    age[0] = 12;
+    age[1] = 4;
+    age[2] = 5;
+    age[3] = 80;
+    age[4] = 18;
+    age[5] = 25;
+    age[6] = 17;
+    age[7] = 20;
+    age[8] = 65;
+    age[9] = 46;
+    System.out.println("array pos 0 : " + age[0]);
+    System.out.println("array pos 1 : " + age[1]);
+    System.out.println("array pos 2 : " + age[2]);
+    System.out.println("array pos 3 : " + age[3]);
+    System.out.println("array pos 4 : " + age[4]);
+    System.out.println("array pos 5 : " + age[5]);
+    System.out.println("array pos 6 : " + age[6]);
+    System.out.println("array pos 7 : " + age[7]);
+    System.out.println("array pos 8 : " + age[8]);
+    System.out.println("array pos 9 : " + age[9]);
+
+    }
+}
