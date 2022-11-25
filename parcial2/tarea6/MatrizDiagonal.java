dd54c12e2c5a0323bb93cdfaa7dd07ab5693b7c3
Emmanuel Reyes <emmanuelrh666@gmail.com>
2022-11-25 14:11:59 -0600

    add programs MatrizDiagonal.com

--- parcial2/tarea6/MatrizDiagonal.java
+++ parcial2/tarea6/MatrizDiagonal.java
@@ -10,8 +10,6 @@ public class MatrizDiagonal{
  
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
- 
-                // Condition for principal diagonal
                 if (i == j) {
                     System.out.print(mat[i][j] + ", ");
                 }
