0b9adf7727de5ad3f0fc3ee0875aeb95383e1f36
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-01-23 22:28:29 -0600

    add ejercevacaciones

---
+++ ejercevacaciones/ejercicios1/HolamundoDos.java
@@ -0,0 +1,8 @@
+package ejercevacaciones.ejercicios1;
+
+public class HolamundoDos {
+    public static void main (String[] args){
+    
+            System.out.print("Hola Mundo, ¿Cómo estas?");
+    }
+}
