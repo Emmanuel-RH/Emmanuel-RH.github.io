181a2c6c5eb6f5bbc28c00cbaa4b72fcf8ea5e8f
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-02-09 06:51:39 -0600

    add HolaMundo.java

--- ejercevacaciones/ejercicios1/HolamundoDos.java
+++ ejercevacaciones/ejercicios1/HolamundoDos.java
@@ -3,6 +3,7 @@ package ejercevacaciones.ejercicios1;
 public class HolaMundoDos {
     public static void main (String[] args){
 
+        //Imprimimos el texto indicado en la función
         System.out.println("Hola Mundo!");
     }
 }
